70aed302b38b487e6503f5ea196f09033959183e
Raif Agovic <raif.agovic.dev@gmail.com>
2024-10-20 18:27:20 +0200

    Initial Commit

---
+++ MyAlarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyAlarm
+//
+//  Created by Raif Agovic on 20. 10. 2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyAlarm/MyAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAlarmApp.swift
+//  MyAlarm
+//
+//  Created by Raif Agovic on 20. 10. 2024..
+//
+
+import SwiftUI
+
+@main
+struct MyAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
