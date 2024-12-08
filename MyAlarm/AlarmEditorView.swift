fdc50f905a1ee573148fe7d6fe417c9b54ad2317
Raif Agovic <raif.agovic.dev@gmail.com>
2024-12-08 20:59:41 +0100

    Change remainingTimeMessage color to A1A1A6

--- MyAlarm/AlarmEditorView.swift
+++ MyAlarm/AlarmEditorView.swift
@@ -35,7 +35,7 @@ struct AlarmEditorView: View {
                            
                         Text(remainingTimeMessage())
                             .font(.headline)
-                            .foregroundColor(.gray)
+                            .foregroundColor(Color(hex: "#A1A1A6"))
                             .padding(.top, 3)
                             .frame(maxWidth: .infinity)
                     }
