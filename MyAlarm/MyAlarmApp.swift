bc0e4fa2554b15ecc11367a0c167133ad022163b
Raif Agovic <raif.agovic.dev@gmail.com>
2024-12-17 19:57:57 +0100

    Remove init function from the MyAlarmApp

--- MyAlarm/MyAlarmApp.swift
+++ MyAlarm/MyAlarmApp.swift
@@ -9,15 +9,6 @@ import SwiftUI
 
 @main
 struct MyAlarmApp: App {
-    init() {
-            // Set the UIWindow background color to black
-            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-                windowScene.windows.forEach { window in
-                    window.backgroundColor = UIColor.black
-                }
-            }
-        }
-    
     var body: some Scene {
         WindowGroup {
             ContentView() // A placeholder because the app's window is managed by AppDelegate
