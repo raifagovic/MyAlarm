164f6ecda58ed2721a76761538d21438ceeda654
Raif Agovic <raif.agovic.dev@gmail.com>
2024-12-31 19:58:30 +0100

    Remove redundant comments

--- MyAlarm/SnoozeView.swift
+++ MyAlarm/SnoozeView.swift
@@ -5,44 +5,6 @@
 //  Created by Raif Agovic on 24. 12. 2024..
 //
 
-//import SwiftUI
-//
-//struct SnoozeView: View {
-//    @Binding var selectedSnooze: Int
-//    
-//    let snoozeOptions = [5, 10, 15, 20, 25, 30]
-//    
-//    var body: some View {
-//        List(snoozeOptions, id: \.self) { option in
-//            HStack {
-//                Text("\(option) minutes")
-//                    .foregroundColor(.white)
-//                Spacer()
-//                if option == selectedSnooze {
-//                    Image(systemName: "checkmark")
-//                        .foregroundColor(Color(hex: "#FFD700"))
-//                }
-//            }
-//            .contentShape(Rectangle()) // Makes the entire row tappable
-//            .onTapGesture {
-//                selectedSnooze = option // Update the selected snooze duration
-//            }
-//            .listRowBackground(Color(hex: "#2C2C2E")) // Set row background color
-//        }
-//        .scrollContentBackground(.hidden)
-//        .background(Color(hex: "#1C1C1E"))
-//        .navigationBarTitleDisplayMode(.inline)
-//        .toolbar {
-//            ToolbarItem(placement: .principal) {
-//                Text("Snooze")
-//                    .foregroundColor(Color(hex: "#F1F1F1"))
-//                    .font(.headline)
-//            }
-//        }
-//    }
-//}
-
-
 import SwiftUI
 
 struct SnoozeView: View {
@@ -74,11 +36,11 @@ struct SnoozeView: View {
                 ToolbarItem(placement: .principal) {
                     Text("Snooze")
                         .font(.headline)
-                        .foregroundColor(Color(hex: "#F1F1F1")) // White color for the title
+                        .foregroundColor(Color(hex: "#F1F1F1"))
                 }
             }
-            .toolbarBackground(Color.clear, for: .navigationBar) // Dark and semi-transparent
-            .toolbarColorScheme(.dark, for: .navigationBar) // Enforces dark theme
+            .toolbarBackground(Color.clear, for: .navigationBar)
+            .toolbarColorScheme(.dark, for: .navigationBar)
         }
     }
 }
