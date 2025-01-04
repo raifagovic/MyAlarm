270f0365edec3c161fe4d9077eca17211754a799
Raif Agovic <raif.agovic.dev@gmail.com>
2025-01-04 19:31:45 +0100

    Remove redundant comment from the MyAlarmApp

--- MyAlarm/MyAlarmApp.swift
+++ MyAlarm/MyAlarmApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MyAlarmApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView() // A placeholder because the app's window is managed by AppDelegate
+            ContentView()
         }
     }
 }
