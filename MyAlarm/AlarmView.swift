de8867e5106ce0fa61864214820229a913f0946c
Raif Agovic <raif.agovic.dev@gmail.com>
2025-01-12 17:16:42 +0100

    Remove redundant .bold attribute from the alarm time

--- MyAlarm/AlarmView.swift
+++ MyAlarm/AlarmView.swift
@@ -42,10 +42,9 @@ struct AlarmView: View {
                 VStack(alignment: .leading) {
                     // Time at the top
                     Text(alarm.time)
-                        .font(.system(size: 55, weight: .light, design: .default))
+                        .font(.system(size: 55, weight: .medium, design: .default))
                         .kerning(-1.5)
                         .foregroundColor(isAlarmOn ? Color(hex: "#F1F1F1") : Color(hex: "#A1A1A6"))
-                        .bold()
                     
                     // Row below time: Symbol • Label • Weekdays
                     HStack {
