d86518e9506f8b12648161395cf299a8746ec9a2
Raif Agovic <raif.agovic.dev@gmail.com>
2025-01-21 16:06:35 +0100

    Create AlarmData class

---
+++ MyAlarm/AlarmData.swift
@@ -0,0 +1,34 @@
+//
+//  AlarmData.swift
+//  MyAlarm
+//
+//  Created by Raif Agovic on 21. 1. 2025..
+//
+
+import SwiftUI
+
+class AlarmData: ObservableObject {
+    @Published var alarms: [Alarm] {
+        didSet {
+            saveAlarms()
+        }
+    }
+
+    init() {
+        self.alarms = AlarmData.loadAlarms()
+    }
+
+    private static func loadAlarms() -> [Alarm] {
+        if let data = UserDefaults.standard.data(forKey: "alarms"),
+           let decoded = try? JSONDecoder().decode([Alarm].self, from: data) {
+            return decoded
+        }
+        return []
+    }
+
+    private func saveAlarms() {
+        if let encoded = try? JSONEncoder().encode(alarms) {
+            UserDefaults.standard.set(encoded, forKey: "alarms")
+        }
+    }
+}
