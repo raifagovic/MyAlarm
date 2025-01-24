b5a429cda43fd5e98e6356862903e397d8049b43
Raif Agovic <raif.agovic.dev@gmail.com>
2025-01-24 23:11:47 +0100

    Integrate AlarmData into MyAlarmApp as a StateObject

--- MyAlarm/MyAlarmApp.swift
+++ MyAlarm/MyAlarmApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct MyAlarmApp: App {
+    @StateObject private var alarmData = AlarmData() // Instantiate AlarmData
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(alarmData)
         }
     }
 }
