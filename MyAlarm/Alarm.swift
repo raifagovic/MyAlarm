3472217e07b489c1cdfe57e03cb590d713a806e4
Raif Agovic <raif.agovic.dev@gmail.com>
2025-02-03 08:33:21 +0100

    Remove 'id' property from the Alarm class

--- MyAlarm/Alarm.swift
+++ MyAlarm/Alarm.swift
@@ -10,19 +10,17 @@ import SwiftData
 
 @Model
 class Alarm {
-    @Attribute(.unique) var id: UUID = UUID()
     var time: Date
     var isOn: Bool
-    var repeatDays: [String]
     var label: String
+    var repeatDays: [String]
     var snoozeDuration: Int
 
-    init(id: UUID, time: Date, isOn: Bool, repeatDays: [String], label: String, snoozeDuration: Int) {
-        self.id = id
+    init(time: Date, isOn: Bool, repeatDays: [String], label: String, snoozeDuration: Int) {
         self.time = time
         self.isOn = isOn
-        self.repeatDays = repeatDays
         self.label = label
+        self.repeatDays = repeatDays
         self.snoozeDuration = snoozeDuration
     }
 }
