f35b5ced2cd07c2e83515c2fdc6e97a8e1e9632d
Raif Agovic <raif.agovic.dev@gmail.com>
2025-02-11 13:45:04 +0100

    Create AlarmUtils.swift

--- MyAlarm/AlarmEditorView.swift
+++ MyAlarm/AlarmEditorView.swift
@@ -49,7 +49,7 @@ struct AlarmEditorView: View {
                             Text("Repeat")
                                 .foregroundColor(Color(hex: "#F1F1F1"))
                             Spacer()
-                            Text(getAbbreviatedDays())
+                            Text(getAbbreviatedDays(from: alarm.repeatDays))
                                 .foregroundColor(Color(hex: "#A1A1A6"))
                                 .lineLimit(1)
                                 .font(.system(size: repeatDays.count >= 6 ? 16.5 : UIFont.preferredFont(forTextStyle: .body).pointSize))
@@ -149,86 +149,6 @@ struct AlarmEditorView: View {
     }
     
     //    Calculate the remaining time and return a formatted string
-    private func remainingTimeMessage() -> String {
-        let calendar = Calendar.current
-        
-        // Get the current time in Sarajevo
-        let now = Date()
-        let currentTimeInSarajevo = now
-        
-        // Calculate the difference
-        let selectedComponents = calendar.dateComponents([.hour, .minute], from: time)
-        
-        // Create a new Date with today's date and selected time
-        var combinedComponents = DateComponents()
-        combinedComponents.year = calendar.component(.year, from: now)
-        combinedComponents.month = calendar.component(.month, from: now)
-        combinedComponents.day = calendar.component(.day, from: now)
-        combinedComponents.hour = selectedComponents.hour
-        combinedComponents.minute = selectedComponents.minute
-        
-        let selectedDate = calendar.date(from: combinedComponents)!
-        
-        let differenceInSeconds = selectedDate.timeIntervalSince(currentTimeInSarajevo)
-        let hours = Int(differenceInSeconds) / 3600
-        let minutes = (Int(differenceInSeconds) % 3600) / 60
-        
-        if differenceInSeconds < 0 {
-            // If the selected time has already passed, show time for the next day
-            let nextDayDate = calendar.date(byAdding: .day, value: 1, to: selectedDate)!
-            let nextDayDifference = nextDayDate.timeIntervalSince(currentTimeInSarajevo)
-            let nextDayHours = Int(nextDayDifference) / 3600
-            let nextDayMinutes = (Int(nextDayDifference) % 3600) / 60
-            return "Rings  in \(nextDayHours) h \(nextDayMinutes) min"
-        }
-        
-        return "Rings  in \(hours) h \(minutes) min"
-    }
     
-    private func getAbbreviatedDays() -> String {
-        let dayAbbreviations: [String: String] = [
-            "Every Monday": "Mon",
-            "Every Tuesday": "Tue",
-            "Every Wednesday": "Wed",
-            "Every Thursday": "Thu",
-            "Every Friday": "Fri",
-            "Every Saturday": "Sat",
-            "Every Sunday": "Sun",
-        ]
-        
-        let daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-        let sortedDays = repeatDays
-            .compactMap { dayAbbreviations[$0] }
-            .sorted { daysOfWeek.firstIndex(of: $0)! < daysOfWeek.firstIndex(of: $1)! }
-        
-        // If no days are selected, return "Never"
-        if sortedDays.isEmpty {
-            return "Never"
-        }
-        
-        // If all days are selected, return "Every"
-        if sortedDays.count == daysOfWeek.count {
-            return "Every day"
-        }
-        
-        // If only one day is selected, return the abbreviation for that day
-        if sortedDays.count == 1 {
-            return sortedDays.first!
-        }
-        
-        if sortedDays == ["Mon", "Tue", "Wed", "Thu", "Fri"] {
-            return "Weekdays"
-        }
-        
-        // Check if weekends are selected
-        if sortedDays == ["Sat", "Sun"] {
-            return "Weekends"
-        }
-        
-        // For multiple selected days, format as "Mon, Tue and Wed"
-        let allButLast = sortedDays.dropLast()
-        let lastDay = sortedDays.last!
-        return "\(allButLast.joined(separator: ", ")) and \(lastDay)"
-    }
 }
 
