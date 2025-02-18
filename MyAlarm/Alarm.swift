9428402d80e1d76960854aba3eb3af0513b1f831
Raif Agovic <raif.agovic.dev@gmail.com>
2025-02-18 08:42:39 +0100

    Add selectedSound to the Alarm class

--- MyAlarm/Alarm.swift
+++ MyAlarm/Alarm.swift
@@ -13,13 +13,15 @@ class Alarm {
     var time: Date
     var label: String
     var repeatDays: [String]
+    var selectedSound: String
     var snoozeDuration: Int
     var isOn: Bool
 
-    init(time: Date, repeatDays: [String], label: String, snoozeDuration: Int, isOn: Bool) {
+    init(time: Date, repeatDays: [String], label: String, selectedSound: String, snoozeDuration: Int, isOn: Bool) {
         self.time = time
         self.label = label
         self.repeatDays = repeatDays
+        self.selectedSound = selectedSound
         self.snoozeDuration = snoozeDuration
         self.isOn = isOn
     }
