d04086a85b3b90a03a33a7bbce98381b44b2b2b2
Raif Agovic <raif.agovic.dev@gmail.com>
2025-02-24 16:16:33 +0100

    Fix: Remove comma after label when no repeat days are selected

--- MyAlarm/AlarmView.swift
+++ MyAlarm/AlarmView.swift
@@ -45,7 +45,7 @@ struct AlarmView: View {
                     
                     // Label and repeat days
                     HStack {
-                        Text((alarm.label.isEmpty ? "Alarm" : alarm.label) + ",")
+                        Text((alarm.label.isEmpty ? "Alarm" : alarm.label) + (alarm.repeatDays.isEmpty ? "" : ","))
                             .foregroundColor(alarm.isOn ? Color(hex: "#F1F1F1") : Color(hex: "#A1A1A6"))
                             .fontWeight(.medium)
                         
