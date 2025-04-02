0f564c85b0e4a37843cac194681eb3cc83c0881b
Raif Agovic <raif.agovic.dev@gmail.com>
2025-04-02 16:03:50 +0200

    Refactor AlarmRingingView to handle dynamic alarm data and sound playback

--- MyAlarm/AlarmRingingView.swift
+++ MyAlarm/AlarmRingingView.swift
@@ -6,48 +6,55 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct AlarmRingingView: View {
-    @Binding var isAlarmRinging: Bool
-    var stopAlarm: () -> Void
-    var snoozeAlarm: () -> Void
-
+    let alarm: Alarm
+    @State private var audioPlayer: AVAudioPlayer?
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         VStack {
-            Text("Alarm Ringing!")
+            Text("Alarm Ringing")
                 .font(.largeTitle)
+                .foregroundColor(.white)
                 .padding()
-
-            Text("Time to wake up!")
+            
+            Text(alarm.label.isEmpty ? "Wake Up!" : alarm.label)
                 .font(.title2)
-
-            Spacer()
-
-            HStack {
-                Button(action: snoozeAlarm) {
-                    Text("Snooze")
-                        .font(.title)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.orange)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-
-                Button(action: stopAlarm) {
-                    Text("Stop")
-                        .font(.title)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.red)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
+                .foregroundColor(.white)
+            
+            Button("Stop Alarm") {
+                stopAlarm()
+                dismiss()
             }
             .padding()
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
         }
-        .background(Color.black.opacity(0.9))
-        .foregroundColor(.white)
-        .edgesIgnoringSafeArea(.all)
+        .background(Color.black.ignoresSafeArea())
+        .onAppear {
+            playSound(named: alarm.selectedSound)
+        }
+        .onDisappear {
+            stopAlarm()
+        }
+    }
+    
+    private func playSound(named sound: String) {
+        guard let url = Bundle.main.url(forResource: sound, withExtension: "mp3") else { return }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.numberOfLoops = -1 // Loop the sound until stopped
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+    }
+    
+    private func stopAlarm() {
+        audioPlayer?.stop()
+        audioPlayer = nil
     }
 }
